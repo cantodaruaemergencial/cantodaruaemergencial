3c30f67dae9f9fd8f8acf2e83a4eee72dae4711c
Matheus Araujo <matheus.saraujo@gmail.com>
2021-06-04 10:40:31 -0300

    data-migration

--- data-migration/Program.cs
+++ data-migration/Program.cs
@@ -9,7 +9,33 @@ namespace data_migration
         {
             char separator = args[1].EndsWith("tsv") ? '\t' : ',';
 
-            if (args[0] == "person")
+            /*if (args[0] == "person" && args[1] == "repeated")
+            {
+                separator = args[2].EndsWith("tsv") ? '\t' : ',';
+                var m = Util.ReadCsv(args[2], separator, 1);
+                var s = new RepeatedPersonMigration().Do(m);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Importados - {s.ImportedLines}");
+                Console.WriteLine($"Nomes vazios - {s.EmptyLines}");
+                Console.WriteLine($"Nomes repetidos - {s.RepeatedNames}");
+                Console.WriteLine($"Números repetidos - {s.RepeatedNumbers}");
+                File.WriteAllText(args[2].Replace("tsv", "sql").Replace("csv", "sql"), s.Query);
+            }*/
+            if (args[0] == "person" && args[1] == "update")
+            {
+                separator = args[2].EndsWith("tsv") ? '\t' : ',';
+                var m = Util.ReadCsv(args[2], separator, 1);
+                var s = new UpdatePersonMigration().Do(m);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"Importados - {s.ImportedLines}");
+                Console.WriteLine($"Nomes vazios - {s.EmptyLines}");
+                Console.WriteLine($"Nomes repetidos - {s.RepeatedNames}");
+                Console.WriteLine($"Números repetidos - {s.RepeatedNumbers}");
+                File.WriteAllText(args[2].Replace("tsv", "sql").Replace("csv", "sql"), s.Query);
+            }
+            else if (args[0] == "person")
             {
                 var m = Util.ReadCsv(args[1], separator, 1);
                 var s = new PersonMigration().Do(m);
