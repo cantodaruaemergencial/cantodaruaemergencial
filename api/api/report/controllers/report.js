6d76c9b79faf17c820a4ceef4b31a87a62089387
Matheus Araujo <matheus.saraujo@gmail.com>
2021-08-06 19:19:51 -0300

    people report

--- api/api/report/controllers/report.js
+++ api/api/report/controllers/report.js
@@ -41,15 +41,65 @@ module.exports = {
       ctx,
       "mensal.csv",
       "select " +
-        "p.cardnumber as numero, p.name as nome, date_format(p.birthdate,'%Y-%m-%d') as nascimento, " +
-        "p.mothername as mae, p.birthplace as naturalidade, p.generalregister as rg, p.cpf as cpf, " +
-        "(select count(1) from person_entrances e " +
-        "where e.person = p.id and month(e.datetime) = :month and year(e.datetime) = :year) as entradas " +
-        "from people p " +
-        "having entradas > 0 " +
-        "order by name;",
+      "p.cardnumber as numero, p.name as nome, date_format(p.birthdate,'%Y-%m-%d') as nascimento, " +
+      "p.mothername as mae, p.birthplace as naturalidade, p.generalregister as rg, p.cpf as cpf, " +
+      "(select count(1) from person_entrances e " +
+      "where e.person = p.id and month(e.datetime) = :month and year(e.datetime) = :year) as entradas " +
+      "from people p " +
+      "having entradas > 0 " +
+      "order by name;",
       params,
       true
     );
   },
+
+  people: async (ctx) => {
+    const params = {
+      from: Number(ctx.query.from),
+      to: Number(ctx.query.to),
+    }
+
+    return getQuery(
+      ctx,
+      "pessoas.csv", 
+      "select " +
+      "p.cardnumber as cartao, " +
+      "ifnull(datediff(date(now()), p.birthdate),0) div 365 as idade, " +
+      "p.birthplace as origem, " +
+      "right(p.birthplace, 2) as estado, " +
+      "sc.SkinColor as cor_raca, " +
+      "g.Gender as sexo, " +
+      "ifnull(p.Childrens,0) as filhos, " +
+      "p.HasHabitation as moradia, " +
+      "datediff(date(now()), p.homelesssince) as tempo_rua_dias, " +
+      "p.HomelessTime as tempo_rua, " +
+      "ifnull(p.HasEmergencyAid, 0) as auxilio_emergencial, " +
+      "ifnull(p.HasPbhBasket, 0) as cesta_pbh, " +
+      "ifnull(p.HasUniqueRegister, 0) as cad_unico, " +
+      "ifnull(p.HasGeneralRegister, 0) as possui_rg, " +
+      "ifnull(p.HasCpf, 0) as possui_cpf, " +
+      "ifnull(p.HasBirthCertificate, 0) as possui_certidao_nascimento, " +
+      "ms.MaritalStatus as estado_civil, " +
+      "st.SchoolTraining as formacao_escolar, " +
+      "(select count(1) from people__benefits pb where pb.person_id = p.id and pb.benefit_id = 1) as bolsa_moradia, " +
+      "(select count(1) from people__benefits pb where pb.person_id = p.id and pb.benefit_id = 2) as bolsa_familia, " +
+      "(select count(1) from people__benefits pb where pb.person_id = p.id and pb.benefit_id = 3) as bpc, " +
+      "(select count(1) from people__external_services pes where pes.person_id = p.id and pes.`external-service_id` = 1) as servico_abrigo, " +
+      "(select count(1) from people__external_services pes where pes.person_id = p.id and pes.`external-service_id` = 2) as servico_albergue, " +
+      "(select count(1) from people__external_services pes where pes.person_id = p.id and pes.`external-service_id` = 3) as servico_centro_pop, " +
+      "(select count(1) from people__external_services pes where pes.person_id = p.id and pes.`external-service_id` = 4) as servico_pastoral_rua, " +
+      "(select count(1) from people__external_services pes where pes.person_id = p.id and pes.`external-service_id` = 5) as servico_abordagem_social, " +
+      "(select count(1) from people__external_services pes where pes.person_id = p.id and pes.`external-service_id` = 6) as servico_republica, " +
+      "(select count(1) from people__external_services pes where pes.person_id = p.id and pes.`external-service_id` = 7) as servico_nenhum_servico " +
+      "from people p " +
+      "left join skin_colors sc on p.skin_color = sc.id " +
+      "left join genders g on p.gender = g.id " +
+      "left join marital_statuses ms on p.marital_status = ms.id " +
+      "left join school_trainings st on p.school_training = st.id " +
+      "where p.cardnumber >= :from and p.cardnumber <= :to " +
+      "order by cast(p.cardnumber as unsigned)",
+      params, 
+      true
+    )
+  }
 };
--- app/services/ReportsService.tsx
+++ app/services/ReportsService.tsx
@@ -3,6 +3,9 @@ import { Api } from '#/packages/api/strapi';
 class ReportsService {
   static getMonthly = (month: string, year: string) =>
     Api.getFile(`reports/monthly?month=${month}&year=${year}`, `mensal-${month}-${year}.csv`);
+
+  static getPeople = (from: string, to: string) =>
+    Api.getFile(`reports/people?from=${from}&to=${to}`, `pessoas-${from}-${to}.csv`);
 }
 
 export default ReportsService;
