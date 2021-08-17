7d2bd41ea4652b2dfebc7994058b3840a2e6af62
Diogo Almazan <diogodpa@outlook.com>
2021-08-16 23:10:44 -0300

    getInitialProps retirado em _document

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,7 +1,5 @@
-import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import React, { ReactElement } from 'react';
-import { ServerStyleSheet as StyledComponentSheets } from 'styled-components';
 
 import DefaultTheme from '#/utils/theme';
 
@@ -69,34 +67,6 @@ const pwaMetaData = (
 );
 
 class TheDocument extends Document {
-  static async getInitialProps(ctx: any) {
-    const styledComponentSheet = new StyledComponentSheets();
-    const materialUiSheets = new MaterialUiServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            styledComponentSheet.collectStyles(
-              materialUiSheets.collect(<App {...props} />),
-            ),
-        });
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: [
-          <React.Fragment key="styles">
-            {initialProps.styles}
-            {materialUiSheets.getStyleElement()}
-            {styledComponentSheet.getStyleElement()}
-          </React.Fragment>,
-        ],
-      };
-    } finally {
-      styledComponentSheet.seal();
-    }
-  }
-
   render(): ReactElement {
     return (
       <Html lang="en">
