a2b3310622ea7bd4008d1b2ebf21f3bfcd367f29
Diogo Almazan <diogodpa@outlook.com>
2023-06-21 21:17:36 -0300

    Fixed seeds behavior

--- api/config/functions/seedsConfig/past-work-categories.js
+++ api/config/functions/seedsConfig/past-work-categories.js
@@ -1,9 +1,9 @@
-const autonomoContrato = require("../../../seeds/past-work-categories/autonomo-contrato.json");
-const bicos = require("../../../seeds/past-work-categories/bicos.json");
-const carteiraAssinada = require("../../../seeds/past-work-categories/carteira-assinada.json");
-const informal = require("../../../seeds/past-work-categories/informal.json");
-const semiescravo = require("../../../seeds/past-work-categories/semiescravo.json");
-const voluntarioSolidario = require("../../../seeds/past-work-categories/voluntario-solidario.json");
+const autonomoContrato = require("../../../seeds/past-work-category/autonomo-contrato.json");
+const bicos = require("../../../seeds/past-work-category/bicos.json");
+const carteiraAssinada = require("../../../seeds/past-work-category/carteira-assinada.json");
+const informal = require("../../../seeds/past-work-category/informal.json");
+const semiescravo = require("../../../seeds/past-work-category/semiescravo.json");
+const voluntarioSolidario = require("../../../seeds/past-work-category/voluntario-solidario.json");
 
 const pastWorkCategoryOptions = [
 	autonomoContrato,
--- api/config/functions/seedsConfig/past-work-sectors.js
+++ api/config/functions/seedsConfig/past-work-sectors.js
@@ -1,9 +1,9 @@
-const comercio = require("../../../seeds/past-work-sectors/comercio.json");
-const industria = require("../../../seeds/past-work-sectors/industria.json");
-const naoSeAplica = require("../../../seeds/past-work-sectors/nao-se-aplica.json");
-const profissionalLiberal = require("../../../seeds/past-work-sectors/profissional-liberal.json");
-const servicos = require("../../../seeds/past-work-sectors/servicos.json");
-const servidorPublico = require("../../../seeds/past-work-sectors/servidor-publico.json");
+const comercio = require("../../../seeds/past-work-sector/comercio.json");
+const industria = require("../../../seeds/past-work-sector/industria.json");
+const naoSeAplica = require("../../../seeds/past-work-sector/nao-se-aplica.json");
+const profissionalLiberal = require("../../../seeds/past-work-sector/profissional-liberal.json");
+const servicos = require("../../../seeds/past-work-sector/servicos.json");
+const servidorPublico = require("../../../seeds/past-work-sector/servidor-publico.json");
 
 const pastWorkSectorOptions = [
 	comercio,
