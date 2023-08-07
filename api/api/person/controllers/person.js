6c0e029a9e31fbcee33d394d0946dde85073d1eb
Diogo Almazan <diogodpa@outlook.com>
2023-08-07 20:27:31 -0300

    Added card number into Person entity and changed some attributes to unique

--- api/api/person/controllers/person.js
+++ api/api/person/controllers/person.js
@@ -1,8 +1,35 @@
-'use strict';
+"use strict";
+
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+	async create(ctx) {
+		const knex = strapi.connections.default;
+
+		let inputEntity = ctx.request.body;
+		console.log({ inputEntity });
+		const nextCardNumber = await knex.raw(
+			"select max(cast(card_number as unsigned)) + 1 as nextCardNumber from person p"
+		);
+		console.log({ k: JSON.stringify(nextCardNumber) });
+		console.log({ aqui: nextCardNumber[0][0]["nextCardNumber"] });
+		if (
+			nextCardNumber &&
+			nextCardNumber[0] &&
+			nextCardNumber[0][0] &&
+			nextCardNumber[0][0]["nextCardNumber"]
+		) {
+			inputEntity.card_number = nextCardNumber[0][0]["nextCardNumber"];
+		} else {
+			inputEntity.card_number = "1";
+		}
+		console.log({ inputEntity });
+		let outputEntity = await strapi.services.person.create(inputEntity);
+		return sanitizeEntity(outputEntity, { model: strapi.models.person });
+	},
+};
