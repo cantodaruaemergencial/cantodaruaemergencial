0a03e63dd5db1e719d68046de9ef474ca48e2e07
Diogo Almazan <diogodpa@outlook.com>
2023-10-30 19:01:39 -0300

    Change database config

--- api/config/database.js
+++ api/config/database.js
@@ -7,12 +7,14 @@ module.exports = ({ env }) => ({
 				client: "mysql",
 				host: env("DATABASE_HOST", "127.0.0.1"),
 				port: env.int("DATABASE_PORT", 3306),
-				database: env("DATABASE_NAME", "cantodarua-db"),
-				username: env("DATABASE_USERNAME", "root"),
-				password: env("DATABASE_PASSWORD", "secret"),
+				database: env("DATABASE_NAME", "strapi"),
+				username: env("DATABASE_USERNAME", ""),
+				password: env("DATABASE_PASSWORD", ""),
 				ssl: env.bool("DATABASE_SSL", true),
 			},
-			options: {},
+			options: {
+				useNullAsDefault: true,
+			},
 		},
 	},
 });
