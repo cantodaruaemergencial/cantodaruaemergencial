e5e625448dba0e7cd951ff60878fb0ae1d906b51
Diogo Almazan <diogodpa@outlook.com>
2023-11-17 21:38:25 -0300

    Testing deploy

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,7 +2,7 @@ import Layout from '#/components/Layout';
 import LoginPage from '#/components/LoginPage';
 
 const IndexPage = (): React.ReactElement => (
-  <Layout title="Canto da Rua - Login">
+  <Layout title="Canto da Rua - Login Teste DEPLOY">
     <LoginPage />
   </Layout>
 );
