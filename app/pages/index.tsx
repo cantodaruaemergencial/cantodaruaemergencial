36ad087816327626a5058d7eba3d9d3cd1f7da2a
Diogo Almazan <diogodpa@outlook.com>
2023-11-17 21:41:45 -0300

    Testing deploy aws

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,7 +2,7 @@ import Layout from '#/components/Layout';
 import LoginPage from '#/components/LoginPage';
 
 const IndexPage = (): React.ReactElement => (
-  <Layout title="Canto da Rua - Login Teste DEPLOY">
+  <Layout title="Canto da Rua - Login Teste DEPLOY AWS">
     <LoginPage />
   </Layout>
 );
